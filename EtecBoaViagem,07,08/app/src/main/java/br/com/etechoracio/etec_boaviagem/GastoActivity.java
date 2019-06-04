c5b4607cd822abf242a6a1504119829d358d00e7
MarisolPoma <marisol.poma15@hotmail.com>
2019-06-04 20:59:18 -0300

    Ex. 19-DATEPICKE -20 Formatação de data

--- EtecBoaViagem,07,08/app/src/main/java/br/com/etechoracio/etec_boaviagem/GastoActivity.java
+++ EtecBoaViagem,07,08/app/src/main/java/br/com/etechoracio/etec_boaviagem/GastoActivity.java
@@ -1,12 +1,23 @@
-    package br.com.etechoracio.etec_boaviagem;
+package br.com.etechoracio.etec_boaviagem;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
-    public class GastoActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+import br.com.etechoracio.etec_boaviagem.utils.DateTimeUtils;
+
+public class GastoActivity extends AppCompatActivity {
+
+    private Button btnData;
+    private Spinner spnTipoGasto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +26,48 @@ import android.widget.Spinner;
 
         spnTipoGasto = (Spinner) findViewById(R.id.spnTipoGasto);
         spnTipoGasto.setAdapter(getAdapterTipoGasto());
-    }
-            private Spinner spnTipoGasto;
 
-        private ArrayAdapter getAdapterTipoGasto() {
-            return new ArrayAdapter<TipoGastoEnum>(this,
-                               R.layout.support_simple_spinner_dropdown_item,
-                                TipoGastoEnum.values());
+        btnData = findViewById(R.id.btnData);
+
     }
+
+    private ArrayAdapter getAdapterTipoGasto() {
+        return new ArrayAdapter<TipoGastoEnum>(this,
+                R.layout.support_simple_spinner_dropdown_item,
+                TipoGastoEnum.values());
+
+    }
+
+    private DatePickerDialog.OnDateSetListener datagasto = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int ano, int mes, int dia) {
+
+            btnData.setText(DateTimeUtils.formatDate(dia, mes +1, ano));
+        }
+    };
+
+    protected Dialog onCreateDialog(int id) {
+        Calendar calendario = Calendar.getInstance();
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        int mes = calendario.get(Calendar.MONTH);
+        int ano = calendario.get(Calendar.YEAR);
+
+        switch (id) {
+            case R.id.btnData:
+                return new DatePickerDialog(this, datagasto, dia, mes, ano);
+        }
+        return null;
+    }
+
+    public void onSelecionarData(View v) {
+        showDialog(v.getId());
+    }
+
 }
+
+
+
+
+
+
+
--- EtecBoaViagem,07,08/app/src/main/java/br/com/etechoracio/etec_boaviagem/ViagemActivity.java
+++ EtecBoaViagem,07,08/app/src/main/java/br/com/etechoracio/etec_boaviagem/ViagemActivity.java
@@ -1,13 +1,77 @@
 package br.com.etechoracio.etec_boaviagem;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import br.com.etechoracio.etec_boaviagem.utils.DateTimeUtils;
+
 
 public class ViagemActivity extends AppCompatActivity {
+
+
+    private Button btnDataChegada;
+    private Button btnDataSaida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viagem);
+
+
+        btnDataChegada = findViewById(R.id.btnDataChegada);
+        btnDataSaida = findViewById(R.id.btnDataSaida);
+
     }
+
+    private void showInsertMessage()  {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmação");
+        builder.setMessage("Registro inserido com sucesso");
+    }
+
+    private DatePickerDialog.OnDateSetListener datachegada = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int ano, int mes, int dia) {
+            btnDataChegada.setText(DateTimeUtils.formatDate(dia, mes + 1, ano));
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener datasaida = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int ano, int mes, int dia) {
+            String data1 = String.valueOf(dia) + "/" + String.valueOf(mes + 1) + "/" + String.valueOf(ano);
+            btnDataSaida.setText(data1);
+        }
+    };
+
+    protected Dialog onCreateDialog(int id){
+        Calendar calendario = Calendar.getInstance();
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+        int mes = calendario.get(Calendar.MONTH);
+        int ano = calendario.get(Calendar.YEAR);
+
+        switch (id) {
+            case R.id.btnDataChegada:
+                return new DatePickerDialog(this, datachegada, dia, mes, ano);
+            case R.id.btnDataSaida:
+                return new DatePickerDialog(this,datasaida,dia,mes,ano);
+        }
+        return null;
+    }
+    public void onSelecionarData(View v){
+        showDialog(v.getId());
+    }
+
 }
+
+
+
+
